f5f7426b25b8e9312c1cead9c50bd8a41a72fa34
K750i <munkin80@gmail.com>
2023-04-17 10:30:59 +0800

    Refactor Content component into Part

--- part1/1.1_courseinfo/src/App.jsx
+++ part1/1.1_courseinfo/src/App.jsx
@@ -36,15 +36,9 @@ function Header({ course }) {
 function Content({ part1, part2, part3, exercises1, exercises2, exercises3 }) {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </>
   )
 }
@@ -52,6 +46,14 @@ function Content({ part1, part2, part3, exercises1, exercises2, exercises3 }) {
 function Total({ exercises1, exercises2, exercises3 }) {
   return (
     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+
+function Part({ part, exercises }) {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
   )
 }
 
