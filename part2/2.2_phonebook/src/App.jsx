7b0cff5d83d052ca8c27fc4142bfade62a127a74
K750i <munkin80@gmail.com>
2023-04-26 10:10:46 +0800

    Setup json-server to serve as dev backend
    - refactor communications with backend to separate module

--- part2/2.2_phonebook/src/App.jsx
+++ part2/2.2_phonebook/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import phoneService from './services/phoneService'
 import Filter from './components/Filter'
 import AddForm from './components/AddForm'
 import Display from './components/Display'
 import './App.css'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-  let nextId = persons.length
+
+  useEffect(() => {
+    phoneService
+      .getAll()
+      .then(persons => {
+        setPersons(persons)
+      })
+  }, [])
 
   return (
     <div>
@@ -42,10 +45,15 @@ function App() {
       return
     }
 
-    setPersons([
-      ...persons,
-      { name: newName, number: newNumber, id: ++nextId }
-    ])
+    phoneService
+      .create({ name: newName, number: newNumber })
+      .then(createdPerson => {
+        setPersons([
+          ...persons,
+          createdPerson
+        ])
+      })
+
     setNewName('')
     setNewNumber('')
   }
---
+++ part2/2.2_phonebook/src/services/phoneService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/persons'
+
+function getAll() {
+  return axios
+    .get(baseURL)
+    .then(response => response.data)
+}
+
+function create(person) {
+  return axios
+    .post(baseURL, person)
+    .then(response => response.data)
+}
+
+export default {
+  getAll,
+  create,
+}
